f291a50c3e382ca6c33e32f8a762dcdab32d52f2
Quyen M. Vo <54432044+voquyen0502@users.noreply.github.com>
2024-04-21 20:37:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.onload = function() {
     // Lấy kích thước của hình ảnh và container
     var image = document.querySelector('.image-container img');
     var container = document.querySelector('.image-container');
-    
+
     // Tính tỉ lệ thu nhỏ
     var scale = Math.min(container.clientWidth / image.naturalWidth, container.clientHeight / image.naturalHeight);
-    
+
     // Áp dụng tỉ lệ thu nhỏ
     image.style.width = image.naturalWidth * scale + 'px';
     image.style.height = image.naturalHeight * scale + 'px';
@@ -27,4 +27,3 @@ window.onload = function() {
         backgroundMusic.play();
     }
 };
-
